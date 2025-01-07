b997dd262a67ad22bc248934b33350522fd6fc29
CodeAldi <codealdi@gmail.com>
2025-01-08 02:06:41 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.dashboard')
+@section('content')
+<div class="row">
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">Hello</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, maxime blanditiis!
+                Maiores numquam earum officiis aliquam, dolores nostrum laborum aut rem nemo maxime tempora consequuntur
+                in placeat explicabo sit optio adipisci unde voluptates? Commodi vitae ducimus illo voluptate repellat
+                esse voluptas accusantium itaque soluta illum maxime molestias, quis ab? Exercitationem!</p>
+        </div>
+    </div>
+</div>
+@endsection
